da28b4d839a4511db8a495ac59ead7ce31ee08f5
kaio-baleeiro <kaio.jesus@bandtec.com.br>
2020-12-26 21:45:11 -0300

    Atualização de script mysql

--- estoque.sql
+++ estoque.sql
@@ -2,14 +2,31 @@ drop database if exists estoque;
 create database estoque;
 use estoque;
 
+create table categoria (
+	idCategoria int primary key auto_increment,
+    categoria varchar(50)
+);
+
+insert into categoria values
+	(null, "Bebidas"),
+    (null, "Alimentos"),
+    (null, "Higiene");
+
 create table produto (
 	idProduto int primary key auto_increment,
     nome varchar(50),
-    categoria varchar(50),
+    fkCategoria int,
     valorProduto double,
-    limiteMin int
+    limiteMin int,
+    foreign key (fkCategoria) references Categoria(idCategoria)
 );
 
+insert into produto values
+	(null, "Coca-cola", 1, 70.90, 100),
+    (null, "Fandangos", 2, 100.00, 50),
+    (null, "Lenço Umedecido", 3, 60.00, 120),
+	(null, "Pepsi", 1, 80.00, 100);
+    
 create table movimentacao (
 	idMovimentacao int primary key auto_increment,
     fkProduto int,
@@ -18,11 +35,7 @@ create table movimentacao (
     foreign key (fkProduto) references produto(idProduto) 
 );
 
-select * from produto;
+select * from categoria;
 select * from movimentacao order by idMovimentacao desc;
-truncate movimentacao;
-
-insert into produto values
-	(null, "Coca-cola", "Bebidas", 70.90, 100),
-    (null, "Fandangos", "Alimentos", 100.00, 50);
+select * from categoria, produto where idCategoria=fkCategoria and categoria = "Bebidas";
 
