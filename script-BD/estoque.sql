9427e8a183382d389100438991020d07e7c7b934
kaio-baleeiro <kaio.jesus@bandtec.com.br>
2021-01-04 14:44:56 -0300

    Organizando diretório de script do banco de dados

--- script-BD/estoque.sql
+++ script-BD/estoque.sql
@@ -4,7 +4,7 @@ use estoque;
 
 create table categoria (
 	idCategoria int primary key auto_increment,
-    categoria varchar(50)
+    nomeCategoria varchar(50)
 );
 
 insert into categoria values
@@ -16,26 +16,29 @@ create table produto (
 	idProduto int primary key auto_increment,
     nome varchar(50),
     fkCategoria int,
+    quantidadeAtual int,
     valorProduto double,
     limiteMin int,
-    foreign key (fkCategoria) references Categoria(idCategoria)
+    foreign key (fkCategoria) references categoria(idCategoria)
 );
 
 insert into produto values
-	(null, "Coca-cola", 1, 70.90, 100),
-    (null, "Fandangos", 2, 100.00, 50),
-    (null, "Lenço Umedecido", 3, 60.00, 120),
-	(null, "Pepsi", 1, 80.00, 100);
+	(null, "Coca-cola", 1, 1000, 70.90, 100),
+    (null, "Fandangos", 2, 300, 100.00, 50),
+    (null, "Lenço Umedecido", 3, 600, 60.00, 120),
+	(null, "Pepsi", 1, 800, 80.00, 100);
     
 create table movimentacao (
 	idMovimentacao int primary key auto_increment,
     fkProduto int,
     quantidade int,
-    momento datetime,
+    momento time,
     foreign key (fkProduto) references produto(idProduto) 
 );
 
 select * from categoria;
-select * from movimentacao order by idMovimentacao desc;
-select * from categoria, produto where idCategoria=fkCategoria and categoria = "Bebidas";
+select * from categoria, produto where idCategoria=fkCategoria and nomeCategoria = "Bebidas";
+select * from movimentacao;
+select * from (select quantidade, nome, momento from movimentacao, produto where nome = "fandangos" and fkProduto=idProduto order by momento desc limit 20) sele order by momento asc;
+
 
