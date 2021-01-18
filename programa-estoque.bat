f4a065434fa50a7a10c6dab84fa7cf97ca072f29
kaio-baleeiro <kaio.jesus@bandtec.com.br>
2021-01-17 21:32:14 -0300

    Subindo apresentação

--- programa-estoque.bat
+++ programa-estoque.bat
@@ -1,3 +1,3 @@
-cd C:\Users\kaiob\OneDrive\Documentos\bandtec\projeto-estoque\projeto-estoque-java\target
+cd projeto-estoque-java\target
 
 java -jar projeto-estoque-java-1.0-SNAPSHOT-jar-with-dependencies.jar
